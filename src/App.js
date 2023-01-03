98a62c386cd481464f27b7a6d2ef0c539c2bb07b
fatihyanik <fatihyanik07@gmail.com>
2023-01-03 18:01:32 +0100

    added banner home and constants folder in client

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import './App.css';
-import Login from './components/account/Login';
-import Home from './components/home/Home';
+import { useState } from 'react';
+
+import { Box } from '@mui/material';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+
+//components
 import DataProvider from './context/DataProvider';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+import Login from './components/account/Login';
+
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  const token = sessionStorage.getItem('accessToken');
+  return isAuthenticated && token ?
+    <>
+      <Header />
+      <Outlet />
+    </> : <Navigate replace to='/account' />
+};
 
 function App() {
+
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
+
   return (
     <DataProvider>
       <BrowserRouter>
-      
-      <div style={{ margin: '64px' }}>
-        <Routes>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/home' element={<Home />}/>
-        </Routes>
-      </div>
+        <Box style={{ marginTop: 64 }}>
+          <Routes>
+            <Route path='/account' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
+
+            <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/' element={<Home />} />
+            </Route>
+
+         
+          </Routes>
+        </Box>
       </BrowserRouter>
     </DataProvider>
   );

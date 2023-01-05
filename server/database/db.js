26873026fe38915f64e57c1820559dd119bc587a
fatihyanik <fatihyanik07@gmail.com>
2023-01-05 15:06:04 +0100

    added server

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const Connection = async (username, password) => {
+    const URL = `mongodb://${username}:${password}@ac-wixblc0-shard-00-00.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-01.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-02.574ok1o.mongodb.net:27017/?ssl=true&replicaSet=atlas-4ru41m-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.log("Error while connecting with the database", error);
+    }
+}
+
+export default Connection;

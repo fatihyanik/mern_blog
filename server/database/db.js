d0f6c0ef0afc6656ec6dd4291d9452251c01a196
fatihyanik <fatihyanik07@gmail.com>
2023-01-05 15:28:07 +0100

    added server

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@ac-wixblc0-shard-00-00.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-01.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-02.574ok1o.mongodb.net:27017/?ssl=true&replicaSet=atlas-4ru41m-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL = `mongodb://fatih:19811988@ac-wixblc0-shard-00-00.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-01.574ok1o.mongodb.net:27017,ac-wixblc0-shard-00-02.574ok1o.mongodb.net:27017/?ssl=true&replicaSet=atlas-4ru41m-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true });
         console.log("Database connected successfully");
